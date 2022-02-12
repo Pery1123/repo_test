7f51c16c601e158f38455b4093fff76ed47febd0
Peryclais <christ.peryclais@gmail.com>
2022-02-12 12:30:10 +0100

    fonction afficher la pyramide d'étoiles

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 # Ceci est un simple test Python pour la config Git
 print("Configuration de Git")
 print("Cette configuration est réussie avec succès")
+
+# Une petite fonction qui affiche la pyramide en étoile
+
+# Définition de la fonction
+def pyramide(n = 12):
+    for i in range(n):
+        print("*"*i)
+
+# Appel de la fonction
+pyramide(10)
